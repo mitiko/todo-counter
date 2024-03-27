f56f0abaae5382ae04f914c1aeeaab8df761d5a3
Dimitar Rusev <mitikodev@gmail.com>
2024-03-28 01:30:53 +0200

    Walk current directory and print filtered files

--- main.js
+++ main.js
@@ -1,16 +1,49 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
+const path = require("path"); // maybe not?
 
-const main = () => {
-    const todoRegex = core.getInput('todo-regex');
-    console.log(`Regex: '${todoRegex}'`);
+async function* walk(directory, filter = (_file) => true) {
+    for await (const d of await fs.promises.opendir(directory)) {
+        const entry = path.join(directory, d.name);
+        if (d.isDirectory())
+            yield* walk(entry, filter);
+        else if (d.isFile() && filter(entry))
+            yield entry;
+    }
+};
+
+const main = async () => {
+    const inputTodoRegex = core.getInput('todo-regex');
+    const inputFilesIncludeRegex = core.getInput('files-include');
+    const inputFilesExcludeRegex = core.getInput('files-exclude');
+    let todoRegex = new RegExp(inputTodoRegex);
+    let filesIncludeRegex = new RegExp(inputFilesExcludeRegex);
+    let filesExcludeRegex = new RegExp(inputFilesIncludeRegex);
+    filesIncludeRegex = /.*\.js$/;
+    filesExcludeRegex = /^node_modules/;
+
+    console.log(`re1: ${todoRegex}`);
+    console.log(`re2: ${filesIncludeRegex}`);
+    console.log(`re3: ${filesExcludeRegex}`);
+
+    const fileIncludeFilter = inputFilesIncludeRegex == '' ?
+        (_) => true :
+        (file) => filesIncludeRegex.test(file);
+    const fileExcludeFilter = inputFilesExcludeRegex == '' ?
+        (_) => false :
+        (file) => filesExcludeRegex.test(file);
+    const fileFilter = (file) => fileIncludeFilter(file) && !fileExcludeFilter(file);
+
+    for await (const file of walk('.', fileFilter))
+        console.log(file);
 
     core.setOutput("count", 0);
     // TODO: count-diff?
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
 };
 
 try {
